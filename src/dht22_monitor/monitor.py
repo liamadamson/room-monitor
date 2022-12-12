f0c4037140bce7b00a19f1c2daed85bc83ed5fa4
Liam Adamson <liamadamson1996@gmail.com>
2022-12-12 22:27:04 +0000

    Make GPIO pin variable

--- src/dht22_monitor/monitor.py
+++ src/dht22_monitor/monitor.py
@@ -19,11 +19,12 @@ class Monitor:
     Used to monitor and print humidity and temperature data from a DHT22 sensor.
     """
 
-    def __init__(self, timestep_s: float) -> None:
+    def __init__(self, timestep_s: float, gpio_pin: int) -> None:
         if timestep_s < 30:
             raise ValueError(f"Timestep must be at least 30 seconds.")
 
         self.timestep_s = timestep_s
+        self._gpio_pin = gpio_pin
 
     def run(self) -> None:
         """
@@ -33,13 +34,13 @@ class Monitor:
             self._step()
 
     def _print_to_screen(self, temp_degc: float, humidity_pc: float) -> None:
-        print(f"Temp: {temp_degc} deg C, Humidity: {humidity_pc} %")
+        print(f"Temperature: {temp_degc} deg C, Humidity: {humidity_pc} %")
 
     def _fetch_data(self, gpio_pin: int) -> DHT22Data:
         humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, gpio_pin)
         return DHT22Data(humidity, temp)
 
     def _step(self) -> None:
-        dht22_data = self._fetch_data(22)
+        dht22_data = self._fetch_data(self._gpio_pin)
         self._print_to_screen(dht22_data.temperature_degc, dht22_data.humidity_percent)
         time.sleep(self.timestep_s)
--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -7,13 +7,13 @@ from dht22_monitor import monitor
 
 @pytest.fixture(name="monitor_instance")
 def fixture_monitor_instance():
-    return monitor.Monitor(30.0)
+    return monitor.Monitor(30.0, 5)  # 30 second timestep. GPIO pin 5.
 
 
 @pytest.mark.parametrize("timestep_s", [-5, 0, 29.9])
 def test_bad_time_step(timestep_s):
     with pytest.raises(ValueError):
-        monitor.Monitor(timestep_s)
+        monitor.Monitor(timestep_s, 5)
 
 
 def test_print_to_screen(monitor_instance, capsys):
@@ -22,7 +22,7 @@ def test_print_to_screen(monitor_instance, capsys):
 
     monitor_instance._print_to_screen(temp_degc, humidity_pc)
 
-    desired_output = f"Temp: {temp_degc} deg C, Humidity: {humidity_pc} %\n"
+    desired_output = f"Temperature: {temp_degc} deg C, Humidity: {humidity_pc} %\n"
 
     captured = capsys.readouterr()
     assert captured.out == desired_output
@@ -47,8 +47,9 @@ def test_fetch_data(monitor_instance, gpio_pin):
 
 
 @pytest.mark.parametrize("timestep_s", [30.0, 60.0, 600.0])
-def test_step(timestep_s):
-    monitor_instance = monitor.Monitor(timestep_s)
+@pytest.mark.parametrize("gpio_pin", [5, 10, 20])
+def test_step(timestep_s, gpio_pin):
+    monitor_instance = monitor.Monitor(timestep_s, gpio_pin)
 
     def fake_fetch_data(*args):
         return monitor.DHT22Data(60.0, 25.0)
@@ -56,6 +57,6 @@ def test_step(timestep_s):
     with patch.object(monitor_instance, "_fetch_data", side_effect=fake_fetch_data), patch("time.sleep"), patch.object(monitor_instance, "_print_to_screen"):
         monitor_instance._step()
 
-        monitor_instance._fetch_data.assert_called_with(22)
+        monitor_instance._fetch_data.assert_called_with(gpio_pin)
         monitor_instance._print_to_screen.assert_called_with(25.0, 60.0)
         time.sleep.assert_called_with(timestep_s)

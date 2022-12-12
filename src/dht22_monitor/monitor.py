d1a061be8e9b0c60b44affe60959f28c32a2d6e2
Liam Adamson <liamadamson1996@gmail.com>
2022-12-12 19:38:16 +0000

    Add fetch data method

--- src/dht22_monitor/monitor.py
+++ src/dht22_monitor/monitor.py
@@ -1,8 +1,23 @@
+import dataclasses
+import Adafruit_DHT
+
+@dataclasses.dataclass
+class DHT22Data:
+    humidity_percent: float
+    temperature_degc: float
+
+
 class Monitor:
     
     def __init__(self, timestep_s) -> None:
         if timestep_s < 30:
             raise ValueError(f"Timestep must be at least 30 seconds.")
 
+    
     def _print_to_screen(self, temp_degc: float, humidity_pc: float) -> None:
         print(f"Temp: {temp_degc} deg C, Humidity: {humidity_pc} %")
+
+
+    def _fetch_data(self, gpio_pin: int):
+        humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, gpio_pin)
+        return DHT22Data(humidity, temp)
--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,5 +1,7 @@
 import pytest
+from unittest.mock import patch
 from dht22_monitor import monitor
+import Adafruit_DHT
 
 
 @pytest.fixture(name="monitor_instance")
@@ -23,3 +25,21 @@ def test_print_to_screen(monitor_instance, capsys):
 
     captured = capsys.readouterr()
     assert captured.out == desired_output
+
+
+@pytest.mark.parametrize("gpio_pin", [3, 6, 10])
+def test_fetch_data(monitor_instance, gpio_pin):
+    humidity_pc = 60.0
+    temperature_degc = 25.0
+    
+    expected = monitor.DHT22Data(humidity_pc, temperature_degc)
+
+    def fake_return(*args):
+        return (humidity_pc, temperature_degc)
+
+    with patch("Adafruit_DHT.read_retry", side_effect=fake_return):
+        data = monitor_instance._fetch_data(gpio_pin)
+
+        Adafruit_DHT.read_retry.assert_called_with(Adafruit_DHT.DHT22, gpio_pin)
+
+    assert data == expected

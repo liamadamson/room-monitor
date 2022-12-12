83494e113ab9186ec4988acc4c6d7a35c504ca35
Liam Adamson <liamadamson1996@gmail.com>
2022-12-12 22:03:57 +0000

    Mount pylintrc into docker during tests

--- scripts/testing/run_pylint.sh
+++ scripts/testing/run_pylint.sh
@@ -1,3 +1,3 @@
 # To be run from the main folder.
 
-docker run --rm -it -v "$PWD/src":"/code/src" dht22-monitor pylint ./src/
+docker run --rm -it -v "$PWD/src":"/code/src" -v "$PWD/.pylintrc":"/code/.pylintrc" dht22-monitor pylint ./src/

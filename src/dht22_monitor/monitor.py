70c84647a27c62eb57bcedc43314b8b4c39a58ff
Liam Adamson <liamadamson1996@gmail.com>
2022-12-12 21:46:10 +0000

    Fix mypy errors

--- src/dht22_monitor/monitor.py
+++ src/dht22_monitor/monitor.py
@@ -1,6 +1,7 @@
 import dataclasses
-import Adafruit_DHT
 import time
+import Adafruit_DHT # type: ignore
+
 
 @dataclasses.dataclass
 class DHT22Data:
@@ -9,25 +10,25 @@ class DHT22Data:
 
 
 class Monitor:
-    
-    def __init__(self, timestep_s) -> None:
+
+    def __init__(self, timestep_s: float) -> None:
         if timestep_s < 30:
             raise ValueError(f"Timestep must be at least 30 seconds.")
 
         self.timestep_s = timestep_s
-    
-    def run(self):
+
+    def run(self) -> None:
         while True:
             self._step()
 
     def _print_to_screen(self, temp_degc: float, humidity_pc: float) -> None:
         print(f"Temp: {temp_degc} deg C, Humidity: {humidity_pc} %")
 
-    def _fetch_data(self, gpio_pin: int):
+    def _fetch_data(self, gpio_pin: int) -> DHT22Data:
         humidity, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, gpio_pin)
         return DHT22Data(humidity, temp)
 
-    def _step(self):
+    def _step(self) -> None:
         dht22_data = self._fetch_data(22)
         self._print_to_screen(dht22_data.temperature_degc, dht22_data.humidity_percent)
         time.sleep(self.timestep_s)

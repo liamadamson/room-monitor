c916435a3ea1fa17983a841f6cae9ce672ac6d2b
Liam Adamson <liamadamson1996@gmail.com>
2022-12-12 19:04:42 +0000

    Add print to screen method

--- src/dht22_monitor/monitor.py
+++ src/dht22_monitor/monitor.py
@@ -3,3 +3,6 @@ class Monitor:
     def __init__(self, timestep_s) -> None:
         if timestep_s < 30:
             raise ValueError(f"Timestep must be at least 30 seconds.")
+
+    def _print_to_screen(self, temp_degc: float, humidity_pc: float) -> None:
+        print(f"Temp: {temp_degc} deg C, Humidity: {humidity_pc} %")
--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,7 +1,25 @@
 import pytest
 from dht22_monitor import monitor
 
+
+@pytest.fixture(name="monitor_instance")
+def fixture_monitor_instance():
+    return monitor.Monitor(30.0)
+
+
 @pytest.mark.parametrize("timestep_s", [-5, 0, 29.9])
 def test_bad_time_step(timestep_s):
     with pytest.raises(ValueError):
         monitor.Monitor(timestep_s)
+
+
+def test_print_to_screen(monitor_instance, capsys):
+    temp_degc = 22.0
+    humidity_pc = 50.0
+
+    monitor_instance._print_to_screen(temp_degc, humidity_pc)
+
+    desired_output = f"Temp: {temp_degc} deg C, Humidity: {humidity_pc} %\n"
+
+    captured = capsys.readouterr()
+    assert captured.out == desired_output

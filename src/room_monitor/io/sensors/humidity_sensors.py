eaea4270e9f78435670f003fbed750514e099d1b
Liam Adamson <liamadamson1996@gmail.com>
2022-12-15 20:58:44 +0000

    Add BME280 humidity sensor

---
+++ src/room_monitor/io/sensors/humidity_sensors.py
@@ -0,0 +1,22 @@
+"""
+Contains sensors for reading the humidity.
+"""
+
+from room_monitor.io.sensors import sensor
+import bme280 # type: ignore
+
+
+class BME280HumiditySensor(sensor.Sensor):
+    """
+    Used to read the humidity, in percentage relative humidity, from a BME280.
+    """
+
+    def __init__(self, bme_280: bme280.BME280) -> None:
+        self._bme_280 = bme_280
+
+    def read(self) -> float:
+        return float(self._bme_280.get_humidity())
+
+    @property
+    def unit(self) -> str:
+        return "% RH"
---
+++ tests/io/sensors/test_humidity_sensor.py
@@ -0,0 +1,18 @@
+from room_monitor.io.sensors import humidity_sensors
+from unittest.mock import Mock
+import pytest
+
+
+@pytest.mark.parametrize("humidity", [40.0, 57.8])
+def test_bme280_humidity_sensor_get_humidity(humidity):
+    mock_bme280 = Mock()
+    mock_bme280.get_humidity.return_value = humidity
+
+    sensor = humidity_sensors.BME280HumiditySensor(mock_bme280)
+    assert sensor.read() == humidity
+
+
+def test_bme280_humidity_sensor_unit():
+    mock_bme280 = Mock()
+    sensor = humidity_sensors.BME280HumiditySensor(mock_bme280)
+    assert sensor.unit == "% RH"

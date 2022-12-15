8347022e9397a69bc074586b7e127792b1a7561d
Liam Adamson <liamadamson1996@gmail.com>
2022-12-15 20:36:47 +0000

    Refactor temperature sensors

--- src/room_monitor/io/sensors/temperature_sensors.py
+++ src/room_monitor/io/sensors/temperature_sensors.py
@@ -2,21 +2,19 @@
 Contains temperature readers.
 """
 
-import smbus2
 import bme280 # type: ignore
 
 
-class BME280TempReader():
+class BME280TemperatureSensor():
     """
     Used to read the temperature, in degrees C, from a BME280 temperature sensor.
     """
 
-    def __init__(self, i2c_bus_no: int = 1) -> None:
-        self._bus = smbus2.SMBus(i2c_bus_no)
-        self._bme280 = bme280.BME280(i2c_dev=self._bus)
+    def __init__(self, bme_280: bme280.BME280) -> None:
+        self._bme_280 = bme_280
 
     def read(self) -> float:
-        return float(self._bme280.get_temperature())
+        return float(self._bme_280.get_temperature())
 
     @property
     def unit(self) -> str:
---
+++ tests/io/sensors/test_temperature_sensors.py
@@ -0,0 +1,18 @@
+from room_monitor.io.sensors import temperature_sensors
+from unittest.mock import Mock
+import pytest
+
+
+@pytest.mark.parametrize("temperature", [18.0, 25.3])
+def test_bme280_temp_reader_get_temp(temperature):
+    mock_bme280 = Mock()
+    mock_bme280.get_temperature.return_value = temperature
+
+    temp_reader = temperature_sensors.BME280TemperatureSensor(mock_bme280)
+    assert temp_reader.read() == temperature
+
+
+def test_bme280_read_unit():
+    mock_bme280 = Mock()
+    reader = temperature_sensors.BME280TemperatureSensor(mock_bme280)
+    assert reader.unit == "degrees C"

28a43e603728c2695274e03783aa5dc9f4154786
Liam Adamson <liamadamson1996@gmail.com>
2022-12-20 17:37:15 +0000

    Add inputs dataclass

---
+++ src/room_monitor/io/inputs.py
@@ -0,0 +1,8 @@
+import dataclasses
+from room_monitor.io.sensors import sensor
+
+
+@dataclasses.dataclass
+class Inputs:
+    room_temperature: sensor.Sensor
+    room_humidity: sensor.Sensor
---
+++ tests/io/test_inputs.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from room_monitor.io import inputs
+import dataclasses
+
+def test_inputs_dataclass_names() -> None:
+    expected_field_names = [
+        "room_temperature",
+        "room_humidity"
+    ]
+
+    actual_field_names =  [field.name for field in dataclasses.fields(inputs.Inputs)]
+
+    assert expected_field_names == actual_field_names

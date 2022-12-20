d481fbd1074445484ab9da4aa49e7ed5ad1834d3
Liam Adamson <liamadamson1996@gmail.com>
2022-12-20 20:05:46 +0000

    Add step method in application

--- src/room_monitor/application.py
+++ src/room_monitor/application.py
@@ -1,4 +1,19 @@
-class Application:  
+from room_monitor.io import inputs
+import time
+
+class Application:
+
+    def step(self, timestep_s, input_dataclass: inputs.Inputs) -> None:
+        temperature = self.get_temperature(input_dataclass)
+        humidity = self.get_humidity(input_dataclass)
+        self._print_temperature_and_humidity(temperature, humidity)
+        time.sleep(timestep_s)
     
+    def get_temperature(self, input_dataclass: inputs.Inputs) -> float:
+        return input_dataclass.room_temperature.read()
+
+    def get_humidity(self, input_dataclass: inputs.Inputs) -> float:
+        return input_dataclass.room_humidity.read()
+
     def _print_temperature_and_humidity(self, temperature_deg_c: float, humidity_rh: float) -> None:
         print(f"Temperature: {temperature_deg_c} degC, Humidity: {humidity_rh} %RH")
--- tests/test_application.py
+++ tests/test_application.py
@@ -1,16 +1,34 @@
 from room_monitor import application
 from room_monitor.io import inputs  
+from room_monitor.io.sensors import fake_sensor
+from unittest.mock import patch
 import pytest
 
 
-def test_print_sensor_data(capsys):
-
-    temperature = 30.0
-    humidity = 45.5
-
+@patch("time.sleep")
+@pytest.mark.parametrize("timestep_s", [10.0, 30.0])
+@pytest.mark.parametrize("temp_degc", [20.0, 35.5])
+@pytest.mark.parametrize("humid_rh", [40.1, 60.4])
+def test_step(mock_time, timestep_s, temp_degc, humid_rh, capsys):
     app = application.Application()
-    app._print_temperature_and_humidity(temperature, humidity)
-    
-    captured = capsys.readouterr()
 
-    assert captured.out == f"Temperature: {temperature} degC, Humidity: {humidity} %RH\n"
+    inputs_dataclass = inputs.Inputs(
+        fake_sensor.FakeSensor(),
+        fake_sensor.FakeSensor()
+    )
+
+    def fake_temp_return():
+        return temp_degc
+
+    def fake_humid_return():
+        return humid_rh
+
+    with patch.object(inputs_dataclass.room_temperature, "read", side_effect = fake_temp_return), \
+        patch.object(inputs_dataclass.room_humidity, "read", side_effect = fake_humid_return):
+
+        app.step(timestep_s, inputs_dataclass)
+
+        captured = capsys.readouterr()
+        assert captured.out == f"Temperature: {temp_degc} degC, Humidity: {humid_rh} %RH\n"
+
+        mock_time.assert_called_with(timestep_s)

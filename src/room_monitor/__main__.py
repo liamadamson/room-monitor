8507cb358644b34e373b9f4bb5f9982c1e166102
Liam Adamson <liamadamson1996@gmail.com>
2022-12-21 21:12:56 +0000

    Update main to new interface

--- src/room_monitor/__main__.py
+++ src/room_monitor/__main__.py
@@ -2,28 +2,28 @@
 Main application entry point.
 """
 
-from typing import Dict
+from typing import List, Any
 from room_monitor import monitor
-from room_monitor.io.sensors import sensor
+from room_monitor.io import input
 from room_monitor.io.sensors import temperature_sensors
 from room_monitor.io.sensors import humidity_sensors
 import bme280 # type: ignore
 import smbus2
 
 
-def get_sensors() -> Dict[str, sensor.Sensor[float]]:
+def get_inputs() -> List[input.Input[Any]]:
     i2c_bus = smbus2.SMBus(1)
     bme280_device = bme280.BME280(i2c_dev=i2c_bus)
 
     # Edit here to add or remove sensors.
-    return {
-        "Temperature": temperature_sensors.BME280TemperatureSensor(bme280_device),
-        "Humidity": humidity_sensors.BME280HumiditySensor(bme280_device)
-    }
+    return [
+        input.Input("Temperature", temperature_sensors.BME280TemperatureSensor(bme280_device)),
+        input.Input("Humidity", humidity_sensors.BME280HumiditySensor(bme280_device))
+    ]
 
 def main() -> None:
-    sensors = get_sensors()
-    room_monitor = monitor.Monitor(sensors, timestep_s=10.0)
+    inputs = get_inputs()
+    room_monitor = monitor.Monitor(inputs, timestep_s=10.0)
 
     while True:
         room_monitor.step()

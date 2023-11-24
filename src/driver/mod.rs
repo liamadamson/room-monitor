08742973d93ea1fb22922d230e1078481b92dc1c
Liam Adamson <liamadamson1996@gmail.com>
2023-11-24 23:09:21 +0000

    Add BME280 logic

---
+++ src/driver/mod.rs
@@ -0,0 +1,32 @@
+#[cfg(target_os = "linux")]
+pub mod linux;
+
+#[cfg(target_os = "linux")]
+pub fn get_bme280_driver() -> anyhow::Result<impl BME280Driver> {
+    Ok(linux::LinuxBME280Driver::new()?)
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn get_bme280_driver() -> anyhow::Result<impl BME280Driver> {
+    let mut driver = MockBME280Driver::new();
+    driver.expect_read().returning(|| {
+        Ok(Readings {
+            temperature: 18.5,
+            pressure: 1013.25,
+            humidity: 45.0,
+        })
+    });
+
+    Ok(driver)
+}
+
+pub struct Readings {
+    pub temperature: f32,
+    pub pressure: f32,
+    pub humidity: f32,
+}
+
+#[mockall::automock]
+pub trait BME280Driver {
+    fn read(&mut self) -> anyhow::Result<Readings>;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+pub mod driver;
+
+use driver::{get_bme280_driver, BME280Driver};
+use std::thread::sleep;
+use std::time::Duration;
+
+const LOOP_RATE_S: u64 = 1;
+
 fn main() {
-    println!("Hello, world!");
+    setup_logging();
+
+    log::info!("Application started");
+
+    let driver = get_bme280_driver().expect("Failed to get BME280 driver");
+    let mut runner = Runner::new(driver);
+
+    loop {
+        if let Err(e) = runner.step() {
+            log::error!("Error steppinf runner: {}", e);
+        }
+
+        sleep(Duration::from_secs(LOOP_RATE_S));
+    }
+}
+
+fn setup_logging() {
+    let mut builder = env_logger::Builder::from_default_env();
+    builder
+        .filter_level(log::LevelFilter::Debug)
+        .target(env_logger::Target::Stdout)
+        .init();
+}
+
+struct Runner<T>
+where
+    T: BME280Driver,
+{
+    driver: T,
+}
+
+impl<T> Runner<T>
+where
+    T: BME280Driver,
+{
+    fn new(driver: T) -> Self {
+        Self { driver }
+    }
+
+    fn step(&mut self) -> anyhow::Result<()> {
+        let readings = self.driver.read()?;
+        log::info!(
+            "{} degC,\t {} hPa,\t {} %",
+            readings.temperature,
+            readings.pressure,
+            readings.humidity
+        );
+        Ok(())
+    }
 }

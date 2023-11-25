b80e78848789706a7340d4e6914aafe21b971499
Liam Adamson <liamadamson1996@gmail.com>
2023-11-25 17:38:31 +0000

    :sparkles: Make measurement rate configurable via env variable

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 pub mod driver;
 
 use driver::{get_bme280_driver, BME280Driver};
+use std::env;
 use std::thread::sleep;
 use std::time::Duration;
 
-const LOOP_RATE_S: u64 = 5 * 60;
+const DEFAULT_MEASUREMENT_RATE_S: u64 = 5 * 60; // 5 minutes.
 
 fn main() {
     setup_logging();
 
     log::info!("Application started");
+
+    let measurement_rate_s = get_measurement_rate();
 
     let driver = get_bme280_driver().expect("Failed to get BME280 driver");
     let mut runner = Runner::new(driver);
@@ -19,7 +22,32 @@ fn main() {
             log::error!("Error steppinf runner: {}", e);
         }
 
-        sleep(Duration::from_secs(LOOP_RATE_S));
+        sleep(Duration::from_secs(measurement_rate_s));
+    }
+}
+
+fn get_measurement_rate() -> u64 {
+    match env::var("MEASUREMENT_RATE_S") {
+        Ok(rate) => match rate.parse::<u64>() {
+            Ok(rate) => {
+                log::info!("Using MEASUREMENT_RATE_S of {}", rate);
+                rate
+            },
+            Err(_) => {
+                log::warn!(
+                    "MEASUREMENT_RATE_S is not a valid integer, using default of {}",
+                    DEFAULT_MEASUREMENT_RATE_S
+                );
+                DEFAULT_MEASUREMENT_RATE_S
+            }
+        },
+        Err(_) => {
+            log::warn!(
+                "MEASUREMENT_RATE_S not set, using default of {}",
+                DEFAULT_MEASUREMENT_RATE_S
+            );
+            DEFAULT_MEASUREMENT_RATE_S
+        }
     }
 }
 
